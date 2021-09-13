a21d56791049efcd1caf8ec7a97c5011ddb9f2ac
yilmaz <yilmazapis@gmail.com>
2021-09-13 02:11:36 -0400

    app.js updated

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
-import App from "./app";
 import { store } from "./state";
 import CellList from "./components/cell-list/cell-list";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
