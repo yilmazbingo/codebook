cda94a7fc5e0862e4ae2952902b4484c8daa1bad
yilmaz <yilmazapis@gmail.com>
2021-09-13 02:06:45 -0400

    bundle error fixed

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,16 +1,16 @@
-import CodeCell from "components/code-cell";
-import React from "react";
-import TextEditor from "components/text-editor";
-import CellList from "components";
+// import CodeCell from "components/code-cell";
+// import React from "react";
+// import TextEditor from "components/text-editor";
+// import CellList from "components";
 
-const App = () => {
-  return (
-    <div>
-      <CellList />
-      {/* <TextEditor /> */}
-      {/* <CodeCell /> */}
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <CellList />
+//       {/* <TextEditor /> */}
+//       {/* <CodeCell /> */}
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
